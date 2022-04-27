c420ba50ffb5a350dd167fcd5b13bb559ae16a1a
tranhuyducseven <tranhuyducseven@gmail.com>
2022-04-27 12:46:37 +0700

    [Feature] Details page: screen, widget&content components

---
+++ components/Content/index.tsx
@@ -0,0 +1,54 @@
+import details01 from "@public/details01.png";
+import Image from "next/image";
+
+interface IContentComponentProps {
+  imageSrc: string;
+  heading: string;
+  subheading: string;
+  paragraphs: string[];
+  techs: string[];
+  scope: string;
+}
+export const ContentComponent: IComponent<IContentComponentProps> = (props) => {
+  const { heading, subheading, paragraphs, techs, scope } = props;
+  return (
+    <>
+      <div className="image mb-5">
+        {/* content.src*/}
+        <Image
+          src={details01}
+          alt="Details_01"
+          height={430}
+          className="rounded-lg"
+          layout="responsive"
+        />
+      </div>
+      <div className="content mb-5 p-1">
+        <h1 className="text-3xl font-bold ">{heading}</h1>
+        <h2 className="text-2xl font-medium ">{subheading}</h2>
+      </div>
+      <div>
+        {paragraphs.map((paragraph, index) => (
+          <p key={index} className="text-base mb-7 text-[#68666C]">
+            {paragraph}
+          </p>
+        ))}
+      </div>
+      <div>
+        <ul className="font-bold list-disc mb-7">
+          Tech stack:
+          {techs.map((tech, index) => (
+            <li key={index} className="ml-5 font-normal">
+              {tech}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <p className="text-base mb-7 text-[#68666C]">
+          <span className="italic"> Scope of work: </span> {scope}
+        </p>
+      </div>
+    </>
+  );
+};
---
+++ components/Widget/Project/index.tsx
@@ -0,0 +1,56 @@
+import React, { useMemo } from "react";
+
+export interface IProjectProps {
+  date?: string;
+  location?: string;
+  client?: string;
+  category?: string;
+  link?: string;
+}
+
+export const ProjectComponent: IComponent<IProjectProps> = ({
+  date,
+  location,
+  client,
+  category,
+  link,
+}) => {
+  return (
+    <div className="project">
+      <ul>
+        <li className="mb-3 text-[#68666c] text-base xl:text-sm 2xl:text-base">
+          <span className="text-black capitalize font-semibold mr-1">
+            Date:{" "}
+          </span>
+          {date}
+        </li>
+        <li className="mb-3 text-[#68666c] text-base xl:text-sm 2xl:text-base">
+          <span className="text-black capitalize font-semibold mr-1">
+            Location:{" "}
+          </span>
+          {location}
+        </li>
+        <li className="mb-3 text-[#68666c] text-base xl:text-sm 2xl:text-base">
+          <span className="text-black capitalize font-semibold mr-1">
+            Client:{" "}
+          </span>
+          {client}
+        </li>
+        <li className="mb-3 text-[#68666c] text-base xl:text-sm 2xl:text-base">
+          <span className="text-black capitalize font-semibold mr-1">
+            Category:{" "}
+          </span>
+          {category}
+        </li>
+        <li className="mb-3 text-[#68666c] text-base xl:text-sm 2xl:text-base">
+          <span className="text-black capitalize font-semibold mr-1 ">
+            Link:{" "}
+          </span>
+          <a href="" className="text-[#0054ff]">
+            {link}
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
