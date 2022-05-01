edbab954be72fa259c9e05295d5598a742b97450
Saintno <tctien342@gmail.com>
2022-05-01 10:27:06 +0700

    [Feature] Detail: Mapping path into json

--- pages/details/[detail].tsx
+++ pages/details/[detail].tsx
@@ -1,5 +1,5 @@
 import { DefaultLayout } from "@layouts/DefaultLayout";
-import { DetailsScreen } from "@screens/app/DetailsScreen";
+import { DetailsScreen } from "@screens/Details";
 
 const Details: IPageComponent = ({}) => {
   return <DetailsScreen />;
