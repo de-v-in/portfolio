08dff6ecd2b50c27c6b496986573a83e83436794
Tran Nguyen <tcdnguyen1997@gmail.com>
2022-05-03 11:35:57 +0700

    Complete refact UI and content

--- screens/Home/ProjectCard.tsx
+++ screens/Home/ProjectCard.tsx
@@ -14,28 +14,31 @@ export const ProjectCard: IComponent<IProjectCardProps> = ({
   projectInfo,
 }) => {
   return (
-    <section className={`${styles.card}`}>
+    <section
+      className={`${styles.card} border-slate-800 hover:border-slate-500 dark:border-slate-400 dark:hover:border-slate-100`}
+    >
       <h2 className="font-semibold lg:text-xl text-base">
         {projectInfo.content.heading.toUpperCase()}
       </h2>
       <p className="!text-sm whitespace-pre-line">
         {projectInfo.content.description}
       </p>
-      <section className="my-8">
+      <section className="my-4">
         <Image
           src={projectInfo.content.imageSrc}
           alt="Details_01"
-          width={400}
-          height={300}
-          className="rounded-lg"
+          width={500}
+          height={250}
           layout="responsive"
         />
       </section>
       <footer className="mt-4">
         <Link href={url} passHref>
-          <button className="rounded-full border-2 active:border-yellow-300 border-slate-300 focus:outline-none text-xl px-4 py-2 text-center">
+          <button
+            className={`bg-slate-800 hover:bg-slate-700 active:bg-slate-900 dark:bg-slate-100 dark:hover:bg-white dark:active:bg-slate-200 rounded-full focus:outline-none text-xl px-4 py-2 text-center`}
+          >
             <svg
-              className="fill-slate-300 active:fill-yellow-300"
+              className={`fill-slate-50 dark:fill-black bg-transparent`}
               width="32"
               height="20"
               viewBox="0 0 60 23"
